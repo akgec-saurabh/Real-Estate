6d3705fbb605211611dea120aaa670fad2096045
Saurabh Kumar <akgec.saurabh@gmail.com>
2022-12-03 22:24:20 +0530

    final final

--- src/components/Input/Date/Date.jsx
+++ src/components/Input/Date/Date.jsx
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import "./date.scss";
 
 const Date = (props) => {
+  const [value, setValue] = useState("2023-12-31");
+
   const onChangeHandler = (e) => {
     props.onChange("date", e.target.value);
+    setValue(e.target.value);
   };
 
   return (
     <div className="date">
       <input
-        value="2023-12-31"
+        value={value}
         onChange={onChangeHandler}
         className="input-date"
         type="date"
